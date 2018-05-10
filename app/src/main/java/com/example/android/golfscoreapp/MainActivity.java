424face9a78be6062540baa318e9d962a480a502
Jon Leverkuhn <JonLeverkuhn@gmail.com>
2018-05-10 15:42:48 -0600

    Add styles. Add colors. Add gradients. Add scaled icon drawables.

--- app/src/main/java/com/example/android/golfscoreapp/MainActivity.java
+++ app/src/main/java/com/example/android/golfscoreapp/MainActivity.java
@@ -3,17 +3,25 @@ package com.example.android.golfscoreapp;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Global variables
     public String phase = "initial";
     public int hole = 1;
+    public String course;
+
+    //Global object variables
+    public View coursesViewGroup;
 
     // Save off key global variables on saveInstanceState
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt("hole" , hole);
+        outState.putString("phase" , phase);
+        outState.putString("course" , course);
     }
 
     //onCreate
@@ -22,12 +30,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initial_layout_portrait);
         rebuildState(savedInstanceState);  //Rebuild vars on a state change
+        buildInitialObjects();
     }
 
     //Rebuild key global variables on a state change
     public void rebuildState(Bundle savedInstanceState) {
         if (savedInstanceState != null) {
             hole = savedInstanceState.getInt("hole", hole);
+            phase = savedInstanceState.getString("phase" , phase);
+            course = savedInstanceState.getString("course" , course);
         }
     }
 
@@ -50,6 +61,16 @@ public class MainActivity extends AppCompatActivity {
                 setContentView(R.layout.initial_layout_landscape);
             }
         }
+    }
+
+    public void buildInitialObjects () {
+        coursesViewGroup=findViewById(R.id.course_selection_view_group);
+    }
+
+    //On click method executed when St. Andrews Castle Course is selected.
+    public void setCourseSACastle (View view) {
+        course="castle";
+        coursesViewGroup.setVisibility(View.GONE);
     }
 
 }
