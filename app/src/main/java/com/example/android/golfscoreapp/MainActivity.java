08446f05b166c52c487bc39af3e7ee00cd2bf712
Jon Leverkuhn <JonLeverkuhn@gmail.com>
2018-05-28 15:11:17 -0600

    Configure screen rotation for the initial layout.

--- app/src/main/java/com/example/android/golfscoreapp/MainActivity.java
+++ app/src/main/java/com/example/android/golfscoreapp/MainActivity.java
@@ -1,25 +1,32 @@
 package com.example.android.golfscoreapp;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //Global variables
     public String phase = "initial";
+    public int initialViewState = 1;
     public int hole = 1;
     public String course;
     public int playerCount = 1;
     public String[] playerTeeAry = new String[5];
     public String[] playerNameAry = new String[5];
+    public int[] playerHandicapAry = new int[5];
 
     //Global object variables
     public ScrollView coursesViewGroup;
@@ -29,17 +36,20 @@ public class MainActivity extends AppCompatActivity {
     public LinearLayout playerTwoInfoView;
     public LinearLayout playerThreeInfoView;
     public LinearLayout playerFourInfoView;
-    public Button beginRound;
+    public LinearLayout beginRoundViewGroup;
 
     // Save off key global variables on saveInstanceState
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt("hole" , hole);
+        outState.putInt("initialViewState" , initialViewState);
         outState.putString("phase" , phase);
         outState.putString("course" , course);
         outState.putInt("playerCount" , playerCount);
-        outState.putStringArray("playerTee" , playerTeeAry);
+        outState.putStringArray("playerTeeAry" , playerTeeAry);
+        outState.putStringArray("playerNameAry" , playerNameAry);
+        outState.putIntArray("playerHandicapAry" , playerHandicapAry);
     }
 
     //onCreate
@@ -48,17 +58,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initial_layout_portrait);
         rebuildState(savedInstanceState);  //Rebuild vars on a state change
-        buildInitialObjects();
+        if (phase.equals("initial")) {
+            buildInitialObjects();
+        }
     }
 
     //Rebuild key global variables on a state change
     public void rebuildState(Bundle savedInstanceState) {
         if (savedInstanceState != null) {
             hole = savedInstanceState.getInt("hole" , hole);
+            initialViewState = savedInstanceState.getInt("initialViewState");
             phase = savedInstanceState.getString("phase" , phase);
             course = savedInstanceState.getString("course" , course);
             playerCount = savedInstanceState.getInt("playerCount" , playerCount);
-            playerTeeAry = savedInstanceState.getStringArray("playerTee");
+            playerTeeAry = savedInstanceState.getStringArray("playerTeeAry");
+            playerNameAry = savedInstanceState.getStringArray("playerNameAry");
+            playerHandicapAry = savedInstanceState.getIntArray("playerHandicapAry");
             if (phase.equals("initial")) {
                 buildInitialObjects();
             }
@@ -76,12 +91,14 @@ public class MainActivity extends AppCompatActivity {
     public void changeLayoutBasedOnOrientation (Configuration newConfig) {
         //Portrait orientation
         if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            if (phase.equals("inital")) {
+            if (phase.equals("initial")) {
                 setContentView(R.layout.initial_layout_portrait);
+                buildInitialObjects();
             }
         } else if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            if (phase.equals("inital")) {
+            if (phase.equals("initial")) {
                 setContentView(R.layout.initial_layout_landscape);
+                buildInitialObjects();
             }
         }
     }
@@ -95,21 +112,41 @@ public class MainActivity extends AppCompatActivity {
         playerTwoInfoView=findViewById(R.id.player_2_info);
         playerThreeInfoView=findViewById(R.id.player_3_info);
         playerFourInfoView=findViewById(R.id.player_4_info);
-        beginRound=findViewById(R.id.begin_round);
+        beginRoundViewGroup=findViewById(R.id.button_layout);
+        switch (initialViewState) {
+            case 1:
+                returnToCourseSelection(null);
+                break;
+            case 2:
+                afterCourseSelection();
+                break;
+        }
     }
 
     //On click method executed when a course is selected. Sets visibilities of views.
     public void afterCourseSelection () {
+        initialViewState = 2;
         coursesViewGroup.setVisibility(View.GONE);
         playerCountViewGroup.setVisibility(View.VISIBLE);
         playerInfoViewGroup.setVisibility(View.VISIBLE);
-        beginRound.setVisibility(View.VISIBLE);
+        beginRoundViewGroup.setVisibility(View.VISIBLE);
+    }
+
+    public void returnToCourseSelection (View view) {
+        initialViewState = 1;
+        coursesViewGroup.setVisibility(View.VISIBLE);
+        playerCountViewGroup.setVisibility(View.GONE);
+        playerInfoViewGroup.setVisibility(View.GONE);
+        beginRoundViewGroup.setVisibility(View.GONE);
     }
 
     public void incrementPlayers (View view) {
         if (playerCount < 4) {
             playerCount = playerCount + 1;
             playerTeeAry[0] = String.valueOf(playerCount);
+            playerNameAry[0] = String.valueOf(playerCount);
+            playerHandicapAry[0] = playerCount;
+            saveEditTextVars();
             playerCountView.setText(String.valueOf(playerCount));
             switch (playerCount) {
                 case 2:
@@ -129,7 +166,9 @@ public class MainActivity extends AppCompatActivity {
         if (playerCount > 1) {
             playerCount = playerCount - 1;
             playerTeeAry[0] = String.valueOf(playerCount);
-            playerTeeAry[playerCount+1] = null;
+            playerNameAry[0] = String.valueOf(playerCount);
+            playerHandicapAry[0] = playerCount;
+            saveEditTextVars();
             playerCountView.setText(String.valueOf(playerCount));
             switch (playerCount) {
                 case 1:
@@ -153,6 +192,23 @@ public class MainActivity extends AppCompatActivity {
         teeRadioGroup.clearCheck();
     }
 
+    public void editTextOnClick (View view) {
+        saveEditTextVars();
+    }
+
+    public void saveEditTextVars () {
+        for (int i = 1; i <= playerCount; i++) {
+            EditText name = findViewById(getResources().getIdentifier("player_" + i + "_name", "id", getPackageName()));
+            playerNameAry[i] = name.getText().toString();
+            EditText handicapInput = findViewById(getResources().getIdentifier("player_" + i + "_handicap", "id", getPackageName()));
+            int handicap = Integer.parseInt(handicapInput.getText().toString());
+            if (handicap > 36.4) {
+                Toast.makeText(this,"Per the USGA, the maximum handicap is 36.4.", Toast.LENGTH_SHORT).show();
+            }
+            playerHandicapAry[i] = handicap;
+        }
+    }
+
     public void setPlayerTee (View view) {
         for (int i = 1; i <= playerCount; i++) {
             RadioButton hard = findViewById(getResources().getIdentifier("player_" + i + "_tee_hard", "id", getPackageName()));
@@ -169,6 +225,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void beginRound(View view) {
+        for (int i = 1; i <= playerCount; i++) {
+            if (playerHandicapAry[i] > 36.4) {
+                Toast.makeText(this,"Per the USGA, the maximum handicap is 36.4.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        saveEditTextVars();
     }
 
     //On click method executed when St. Andrews Old Course is selected.
